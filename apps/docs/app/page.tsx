ec70b71024487b6103a71d7ebc14bfc5a466b7bc
Dillon McCloud <dillon.mccloud2@gmail.com>
2026-02-25 14:51:53 -0700

    clean up

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -67,21 +67,14 @@ export default function Page() {
               />
             </div>
             <div className="absolute z-50 flex items-center justify-center w-64 h-64">
-              <Gradient
-                className="opacity-90 w-[120px] h-[120px]"
-                conic
-                small
-              />
+              <Gradient className="opacity-90 w-[120px] h-[120px]" small />
             </div>
 
             <div className="flex justify-center items-center z-50">
               <TurborepoLogo />
             </div>
           </div>
-          <Gradient
-            className="top-[-500px] opacity-[0.15] w-[1000px] h-[1000px]"
-            conic
-          />
+          <Gradient className="top-[-500px] opacity-[0.15] w-[1000px] h-[1000px]" />
           <div className="z-50 flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6">
             <svg
               className="w-[160px] md:w-[200px] fill-black dark:fill-white"
