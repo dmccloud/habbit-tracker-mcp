564de63a4ffb387083b38f390cc0e2f707ff27d1
Dillon McCloud <dillon.mccloud2@gmail.com>
2026-02-26 09:14:35 -0700

    remove old href prop in docs card

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -98,8 +98,8 @@ export default function Page() {
       </div>
 
       <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        {LINKS.map(({ title, href, description }) => (
-          <Card href={href} key={title} title={title}>
+        {LINKS.map(({ title, description }) => (
+          <Card key={title} title={title}>
             {description}
           </Card>
         ))}
