f720fa91f492f25356b8d3767cbc376160362388
Dillon McCloud <dillon.mccloud2@gmail.com>
2026-02-26 09:12:29 -0700

    fix card in docs app

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
+import { Card } from "@repo/ui/components/card";
 import { Gradient } from "@repo/ui/gradient";
 import { TurborepoLogo } from "@repo/ui/turborepo-logo";
 
