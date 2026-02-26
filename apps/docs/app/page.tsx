55b98768cfb1e3d0d53eab18b5c19efcf2d481ea
Dillon McCloud <dillon.mccloud2@gmail.com>
2026-02-26 09:20:22 -0700

    correct card for docs

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/components/card";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@repo/ui/components/card";
 import { Gradient } from "@repo/ui/gradient";
 import { TurborepoLogo } from "@repo/ui/turborepo-logo";
 
@@ -99,8 +104,11 @@ export default function Page() {
 
       <div className="grid mb-32 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         {LINKS.map(({ title, description }) => (
-          <Card key={title} title={title}>
-            {description}
+          <Card key={title}>
+            <CardHeader>
+              <CardTitle>{title}</CardTitle>
+            </CardHeader>
+            <CardContent>{description}</CardContent>
           </Card>
         ))}
       </div>
